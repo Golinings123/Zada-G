1c3421ab20d14e923db835bf46674f8ac61cb442
Golinings <golinings@gmail.com>
2023-01-16 00:16:45 +0530

    first Commit

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Zada</title>
+    <link rel="stylesheet" href="styles/bootstrap-337.min.css">
+    <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+      
+     <div id="top">
+     <div class="container">
+     <div class="col-md-6 offer"> 
+   
+      <a href="#" class="btn btn-success btn-sm">Welcome</a>
+      <a href="#"> 0 Items In Your Cart | Total Price: Rs.0,00.</a> 
+
+     </div>
+
+     <div class"col-md-6"><!---begin--->
+         
+      <ul class="menu">
+       <li>
+             <a href="#">Register</a>
+        </li>
+       <li>
+          <a href="#">My Account</a>
+       </li>
+       <li>
+        <a href="#">Go to Cart</a>
+       </li>
+       <li>
+       <a href="#">Login</a>
+       </li>
+       </ul>
+   </div>
+  </div>
+ </div> <!--- top end--->
+
+ <div class="container" id="slider">
+       
+       <div class="col-md-12">
+           
+           <div class="carousel slide" id="myCarousel" data-ride="carousel">
+               
+               <ol class="carousel-indicators">
+                   
+                   <li class="active" data-target="#myCarousel" data-slide-to="0"></li>
+                   <li data-target="#myCarousel" data-slide-to="1"></li>
+                   <li data-target="#myCarousel" data-slide-to="2"></li>
+                   <li data-target="#myCarousel" data-slide-to="3"></li>
+                   
+               </ol><!-- carousel-indicators Finish -->
+               
+               <div class="carousel-inner">
+                   
+                   <div class="item active">
+                       
+                       <img src="admin_area/slides_images/slider1.jpg" alt="Slider Image 1">
+                       
+                   </div>
+                   
+                   <div class="item">
+                       
+                       <img src="admin_area/slides_images/slider2.jpg" alt="Slider Image 2">
+                       
+                   </div>
+                   
+                   <div class="item">
+                       
+                       <img src="admin_area/slides_images/slider3.jpg" alt="Slider Image 3">
+                       
+                   </div>
+                   
+                   
+                       
+                   </div>
+                   
+               </div><!-- carousel-inner Finish -->
+               
+               <a href="#myCarousel" class="left carousel-control" data-slide="prev">
+                   
+                   <span class="glyphicon glyphicon-chevron-left"></span>
+                   <span class="sr-only">Previous</span>
+                   
+               </a>
+               
+               <a href="#myCarousel" class="right carousel-control" data-slide="next">
+                   
+                   <span class="glyphicon glyphicon-chevron-right"></span>
+                   <span class="sr-only">Next</span>
+                   
+               </a>
+               
+           </div>
+           
+       </div>
+       
+   </div>
+    
+    <script src="js/jquery-331.min.js"></script>
+    <script src="js/bootstrap-337.min.js"></script>
+    
+    
+</body>
+</html>
